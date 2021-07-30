36637ce170ea513f75d8ccf51c4c1c5d73e3721a
Joepolymath <joshuaajagbe96@gmail.com>
2021-07-30 11:48:13 +0100

    i just typed rubbish

--- test1.py
+++ test1.py
@@ -1,3 +1,7 @@
 # this is the first file here
 
 print('hello world')
+
+# this is my new branch. And I am happy to be the branch guy
+
+print('I love backend')
--- test2.py
+++ test2.py
@@ -1,3 +1,6 @@
 # this is the second py file
 
 print('I am awesome')
+
+# this isthe second file on the new branch
+print('I hate backend')
